6ff4f4317138f4e4cc5bc4ce91fee2150788faea
Evgeniy <evgnec@gmail.com>
2024-05-29 19:39:04 +0300

    upd gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+const elements = galleryItems.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+).join("");
+listEl.insertAdjacentHTML("beforeend", elements);
+
+
+// listEl2.style.display = "flex";
+// listEl2.style.gap = "60px";
+// listEl2.style.listStyle = "none";
+// listEl2.style.flexWrap = 'wrap';
+
