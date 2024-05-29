e9c2b6b68c82256f5990d9032b89317a448ffa12
Evgeniy <evgnec@gmail.com>
2024-05-29 23:30:44 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-
 const listEl = document.querySelector(".gallery");
-const elements = galleryItems.map(({ preview, original, description }) =>
-  `<li class="gallery__item">
+const elements = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -14,12 +15,26 @@ const elements = galleryItems.map(({ preview, original, description }) =>
     />
   </a>
 </li>`
-).join("");
+  )
+  .join("");
 listEl.insertAdjacentHTML("beforeend", elements);
 
-
-// listEl2.style.display = "flex";
-// listEl2.style.gap = "60px";
-// listEl2.style.listStyle = "none";
-// listEl2.style.flexWrap = 'wrap';
-
+listEl.addEventListener("click", selectImage);
+function selectImage(e) {
+  if (e.target.nodeName !== "IMG") {
+    console.log("e.target", e.target);
+    return;
+  }
+  console.log("e.target", e.target);
+  const selectImg = e.target.dataset.source;
+  console.log("selectImg ", selectImg);
+  basicLightbox
+    .create(
+      `
+      <div class="modal">
+		<img width="1400" height="900" src="${selectImg}">
+        </div>`
+    )
+      .show();
+    basicLightbox.close();
+}
